9ff3dc4f80efea85d8e7910d42946a4d0b803c09
gustavogorges <gustavo_gorges@hotmail.com>
2024-06-02 19:22:12 -0300

    feat : add getUserByEmail and getAll users on the api

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,3 +13,27 @@ exports.createUser = async (req, res) => {
         return res.status(500).json({ message: 'Failed to register user', error: error.message });
     }
 };
+
+exports.getAllUSers = async (req, res) => {
+    try {
+        const users = await User.findAll();
+        res.status(200).json(users);
+    } catch(error) {
+        return res.status(500).json({ message: 'Failed to fetch users', error: error.message})
+    }
+}
+
+exports.getUserByEmail = async (req, res) => {
+    try {
+        const email = req.params.email;
+        const user = await User.findOne({ where: { email }});
+        if(user){
+            res.status(200).json(user);
+        }
+        else {
+            res.status(404).json({ error : 'User not found'})
+        }
+    } catch(error) {
+        return res.status(500).json({ message: 'Failed to get the specific user', error: error.message})
+    }
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,7 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/users', userController.createUser);
+router.get('/users', userController.getAllUSers);
+router.get('/users/email/:email', userController.getUserByEmail);
 
 module.exports = router;
